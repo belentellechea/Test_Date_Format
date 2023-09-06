967e1583fc6080c374ae43454910ed778bd5bd4d
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2023-09-06 10:16:39 -0300

    tests realizados

---
+++ tests/Library.Tests/DateFormatterTest.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace TestDateFormat.Tests;
+
+public class DateFormatterTests
+{
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    [Test]
+    public void CorrectFormatTest()
+    {
+        string testDate1 = "20/12/2004";
+        string result = DateFormatter.ChangeFormat(testDate1);
+        Assert.AreEqual("2004-12-20", result);
+    }
+
+    [Test]
+    public void IncorrectFormatTest()
+    {
+        string testDate2 = "2004.12.20";
+        string result2 = DateFormatter.ChangeFormat(testDate2);
+        Assert.AreEqual("2004-12-20", result2);
+    }
+
+    [Test]
+    public void EmptyTest()
+    {
+        string testDate3 = "";
+        string result3 = DateFormatter.ChangeFormat(testDate3);
+        Assert.AreEqual("--", result3);
+    }
+
+}
